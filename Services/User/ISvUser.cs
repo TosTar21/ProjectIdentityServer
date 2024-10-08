b55797d2d2ef556cb8be0fb9d37ad1a8085ead0a
TosTar26 <ozsanchez2004@gmail.com>
2024-10-08 07:38:46 -0600

    Reogarnzation code

--- Services/User/ISvUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Services.User
-{
-    public interface ISvUser
-    {
-        Task<(bool Succeeded, string[] Errors)> CreateUserAsync(string userName, string email, string password, int roleId);
-
-}
-}
